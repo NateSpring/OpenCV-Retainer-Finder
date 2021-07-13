cb7157419313c770a45ef6bb61cec7517d0ad354
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2021-07-13 11:03:23 -0600

    Updating to Ver. 2 of Retainer Finder.

--- retainer-finder-GUI.py
+++ retainer-finder-GUI.py
@@ -9,16 +9,20 @@ from PIL import ImageGrab
 from mss import mss
 
 # Mention the installed location of Tesseract-OCR in your system
+# Production
 pytesseract.pytesseract.tesseract_cmd = (
-    r"C:/Users/NateS/AppData/Local/Programs/Tesseract-OCR/tesseract"
+    r"C:/Users/shipping3/AppData/Local/Programs/Tesseract-OCR/tesseract"
 )
-
+# DEV
+# pytesseract.pytesseract.tesseract_cmd = (
+#     r"C:/Users/NateS/AppData/Local/Programs/Tesseract-OCR/tesseract"
+# )
 ########### GUI ###############################################
 sg.theme("Black")
 layout = [
     [
         sg.Text(
-            "Dolly Cell CV",
+            "Dolly Vision",
             size=(15, 1),
             font=("Helvetica", 20),
             justification="center",
@@ -28,27 +32,6 @@ layout = [
         sg.Image(filename="", key="video"),
         sg.Image(filename="", key="video2"),
         sg.Image(filename="", key="video3"),
-    ],
-    [
-        sg.Text(
-            "Brightness",
-            font=("helvetica", 20),
-        ),
-        sg.Slider((0, 255), 100, 1, orientation="h", size=(40, 15), key="-BRIGHTNESS-"),
-    ],
-    [
-        sg.Text(
-            "KNN",
-            font=("helvetica", 20),
-        ),
-        sg.Slider((0, 255), 15, 1, orientation="h", size=(40, 15), key="-KNN-"),
-    ],
-    [
-        sg.Text(
-            "Scale",
-            font=("helvetica", 20),
-        ),
-        sg.Slider((0, 1000), 200, 1, orientation="h", size=(40, 15), key="-SCALE-"),
     ],
     [
         sg.Text(
@@ -105,7 +88,7 @@ layout = [
             "Waiting...",
             font=("Helvetica", 16),
             background_color=("#328ba8"),
-            size=(7, 1),
+            size=(15, 1),
             justification="center",
             key="output",
         ),
@@ -113,27 +96,28 @@ layout = [
     ],
 ]
 # create the window and show it without the plot
-window = sg.Window("OpenCV Integration", layout, no_titlebar=False, location=(0, 0))
+window = sg.Window("Dolly Vision", layout, no_titlebar=False, location=(0, 0))
 
 
 ################################################################
-path = "retainer630-547.xml"  # PATH OF THE CASCADE
-cameraNo = 0  # CAMERA NUMBER
+path = "retainer-712.xml"  # PATH OF THE CASCADE
 objectName = "Retainer Found"  # OBJECT NAME TO DISPLAY
 frameWidth = 640  # DISPLAY WIDTH
 frameHeight = 480  # DISPLAY HEIGHT
 color = (0, 255, 0)
 #################################################################
-cap = cv2.VideoCapture(cameraNo)
-cap2 = cv2.VideoCapture(cameraNo)
+cap = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(1)
+cap3 = cv2.VideoCapture(2)
 
 cap.set(3, frameWidth)
 cap.set(4, frameHeight)
 cap2.set(3, frameWidth)
 cap2.set(4, frameHeight)
+cap3.set(3, 400)
+cap3.set(4, 300)
 
 
-# cv2.namedWindow("Result")
 # LOAD THE CLASSIFIERS DOWNLOADED
 cascade = cv2.CascadeClassifier(path)
 
@@ -146,7 +130,6 @@ serial_detected = False
 def cam1():
     global retainer1_detected
     global frame1
-    global frame2
 
     # SET CAMERA BRIGHTNESS
     event, values = window.read(timeout=20)
@@ -167,40 +150,48 @@ def cam1():
     # DISPLAY THE DETECTED OBJECTS
     if len(objects) > 0:
         window["cam-1"].update("PASS", background_color="green")
-        window["cam-2"].update("PASS", background_color="green")
         retainer1_detected = True
 
     else:
         window["cam-1"].update("Fail", background_color="red")
-        window["cam-2"].update("Fail", background_color="red")
         retainer1_detected = False
-
-    for (x, y, w, h) in objects:
-        area = w * h
-        minArea = cv2.getTrackbarPos("Min Area", "Result")
-        if area > minArea:
-            cv2.rectangle(img, (x, y), (x + w, y + h), color, 3)
-            cv2.putText(
-                img,
-                objectName,
-                (x, y - 5),
-                cv2.FONT_HERSHEY_COMPLEX,
-                1,
-                color,
-                2,
-            )
     imgbytes = cv2.imencode(".png", img)[1].tobytes()
     frame1 = img
-    frame2 = img
     window["video"].update(data=imgbytes)
-    window["video2"].update(data=imgbytes)
 
 
 # right side cam
 def cam2():
     global retainer2_detected
-    # window["video3"].update(data=imgbytes)
-    retainer2_detected = True
+    global frame2
+
+    # SET CAMERA BRIGHTNESS
+    event, values = window.read(timeout=20)
+    if values["-BRIGHTNESS-"]:
+        cap2.set(10, values["-BRIGHTNESS-"])
+    if values["-KNN-"]:
+        neighSlider = int(values["-KNN-"])
+    if values["-SCALE-"]:
+        scale = int(values["-SCALE-"])
+
+    # GET CAMERA IMAGE AND CONVERT TO GRAYSCALE
+    success, img = cap2.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # DETECT THE OBJECT USING THE CASCADE
+    scaleVal = 1 + (scale / 1000)
+    neig = neighSlider
+    objects = cascade.detectMultiScale(gray, scaleVal, neig)
+    # DISPLAY THE DETECTED OBJECTS
+    if len(objects) > 0:
+        window["cam-2"].update("PASS", background_color="green")
+        retainer2_detected = True
+
+    else:
+        window["cam-2"].update("Fail", background_color="red")
+        retainer2_detected = False
+    imgbytes = cv2.imencode(".png", img)[1].tobytes()
+    frame2 = img
+    window["video2"].update(data=imgbytes)
 
 
 # serial cam
@@ -209,34 +200,20 @@ def cam3():
     global serialNumber
     global frame3
 
+    ##screen cap stuff
     # GET CAMERA IMAGE AND CONVERT TO GRAYSCALE
-    bounding_box = {"top": 100, "left": 25, "width": 400, "height": 300}
-    sct = mss()
-    sct_img = sct.grab(bounding_box)
-    frame = np.array(sct_img)
+    # bounding_box = {"top": 100, "left": 25, "width": 400, "height": 300}
+    # sct = mss()
+    # sct_img = sct.grab(bounding_box)
+    # frame = np.array(sct_img)
+
+    success, frame = cap3.read()
 
     # Read image from which text needs to be extracted
     d = pytesseract.image_to_data(frame, output_type=Output.DICT)
     n_boxes = len(d["text"])
     for i in range(n_boxes):
         if int(float(d["conf"][i])) > 30:
-            # (x, y, w, h) = (
-            #     d["left"][i],
-            #     d["top"][i],
-            #     d["width"][i],
-            #     d["height"][i],
-            # )
-
-            # padding = 5
-            # cv2.rectangle(
-            #     frame,
-            #     (x - padding, y - padding),
-            #     (x + w + padding, y + h + padding),
-            #     (0, 255, 0),
-            #     2,
-            # )
-            # if d["text"][i] != "":
-            #     print(d["text"])
             if "SN:" in d["text"][i]:
                 (x, y, w, h) = (
                     d["left"][i],
@@ -269,7 +246,7 @@ def cam3():
                 )
                 # print(d["text"][i], d["text"][i + 1])
                 window["cam-serial"].update(
-                    "{}{}".format(d["text"][i], d["text"][i + 1]),
+                    "{}".format(d["text"][i + 1]),
                     background_color="green",
                 )
                 serialNumber = d["text"][i + 1]
